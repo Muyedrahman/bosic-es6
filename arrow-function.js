f8de766af117f341ef2b06e0fb7c53bf5829df25
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-03 15:04:00 +0600

    Added arrow function

--- arrow-function.js
+++ arrow-function.js
@@ -12,8 +12,12 @@ const addition = function(num1 , num2){
 // arrow function
 const add2 = (num1, num2) => num1 + num2;
 const multiply = (a,b) => a * b;
+const sumAll = (a,b,c,d,e,f) => a+b+c+d+e+f;
+const isFirstBig = (x,y) => x > y;
 
 const result = add2(17, 13);
 // console.log(result)
 const mult = multiply(7, 3);
-console.log(mult)
+// console.log(mult)
+const tr = isFirstBig(7, 30);
+console.log(tr)

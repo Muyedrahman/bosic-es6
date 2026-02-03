3399ef7c8d76ade0e284319a74890279ce4dcbc6
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-03 15:33:19 +0600

    Add multi-line arrow function for total calculation

--- arrow-function.js
+++ arrow-function.js
@@ -15,6 +15,33 @@ const multiply = (a,b) => a * b;
 const sumAll = (a,b,c,d,e,f) => a+b+c+d+e+f;
 const isFirstBig = (x,y) => x > y;
 
+// multi line arrow function
+const doMath = (x, y) => {
+    const makeDouble = x * 2;
+    const againDouble = y*2;
+    const result = makeDouble + againDouble;
+    return result;
+}
+const checkEvenOdd = (num) => {
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+};
+
+console.log(checkEvenOdd(7));
+
+const numbers = [1, 2, 3, 4];
+
+const squaredNumbers = numbers.map((n) => {
+  const square = n * n;
+  return square;
+});
+
+console.log(squaredNumbers);
+
+
 const result = add2(17, 13);
 // console.log(result)
 const mult = multiply(7, 3);
---
+++ more-arrow.js
@@ -0,0 +1,9 @@
+const getTax = (amount, taxRate) => amount * taxRate/100;
+const add = (p,q) => p+ q;
+
+// songle patameter
+const getSquare = (x) => x*x;
+const getHalf = num => num/2;
+console.log(getSquare(8));
+console.log(getHalf(20));
+// 
